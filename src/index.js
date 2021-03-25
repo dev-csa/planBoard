2ad6b884aa95643cd34d7ad9c6e96c1fe52b947e
dev-csa <csarising@naver.com>
2021-03-25 12:08:01 +0900

    Add new project to Firebase collection

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { reduxFirestore, getFirestore } from 'redux-firestore'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 import fbConfig from './config/fbConfig'
 
 const store = createStore(rootReducer, 
   compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore})),
+      reduxFirestore(fbConfig),
+      reactReduxFirebase(fbConfig)
     )
   );
 
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,18 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, {getFirebase, getFirestore}) => {
     // make async call to database
-    dispatch({type: 'CREATE_PROJECT', project});
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: '바둑이',
+      authorLastName: 'Dog',
+      authorId: 123,
+      createAt: new Date()
+    }).then(()=>{
+      dispatch({type: 'CREATE_PROJECT', project});  
+    }).catch((err)=>{
+      dispatch({type: 'CREATE_PROJECT_ERROR', err});
+    })
+    
   }
 };
